39d6fdf719a8109932316a14610e8d28e2352a65
JoseFCosta <jfbritocosta@outlook.com>
2024-06-29 03:07:16 -0300

    muita_ atualizacoes

--- src/homePage.jsx
+++ src/homePage.jsx
@@ -1,211 +1,239 @@
-import { useState } from 'react'
+import { useState } from "react";
 
+import "./index.css";
 
-import banned from './assets/banned2.png'
-import imagemInicio from './assets/Imagem2.svg'
-import instagram from './assets/instagram.png'
-import email from './assets/email.png'
-
-import "./index.css"
+import banned from "./assets/banned2.png";
+import imagemInicio from "./assets/Imagem2.svg";
+import instagram from "./assets/instagram.png";
+import email from "./assets/email.png";
+import imagemInicio2 from "./assets/grupo_de_pessoas.png";
+import mulher_falando from "./assets/mulher_falando.png";
 
 function HomePage() {
-
   return (
     <>
-    <div className="menu">
-      <ul>
-        <li>
-          <a src="/">
-            <b>CAPS - Centro de Apoio Psicossocial</b>
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div className="menu">
+        <ul>
+          <li>
+            <a src="/">
+              <b>CAPS - Centro de Apoio Psicossocial</b>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="container-titulo">
+        <h1>CAPS - O que é isso? </h1>
+        <br />
+      </div>
 
-    <div className="container-titulo">
-      <br />
-      <br />
-      <h1>Introdução</h1>
-      <br />
-      <h3>Não. O CAPS não é um hospicio</h3>
-    </div>
-
-    <div className="container">
-      <table>
-        <tbody>
-            
-        <tr>
-          <td className="td-direita">
-            <div className="apresentacao">
-              <h1>O que são os CAPS?</h1>
-              <br />
-              <p>
-                Os Centros de Apoio Psicossocial (CAPS) desempenham um papel
-                crucial na promoção da saúde mental e no suporte às comunidades.
-                Eles são fundamentais para a desinstitucionalização do
-                tratamento psiquiátrico, oferecendo uma alternativa ao
-                internamento hospitalar. Os CAPS proporcionam um ambiente
-                acolhedor e humanizado, onde os pacientes podem receber
-                tratamento contínuo e integrado. Além disso, os CAPS trabalham
-                para combater o estigma associado às doenças mentais, promovendo
-                a inclusão social e o fortalecimento dos vínculos familiares e
-                comunitários
-              </p>
-              <br />
-            </div>
-          </td>
-          <td className="td-esquerda"><img src={imagemInicio} /></td>
-        </tr>
-        <td className="td-direita">
-          <h1> Aqui vai ter uma imagem</h1>
-        </td>
-        <td className="td-esquerda">
-        <div className="apresentacao">
-              <h1>E qual é a sua importância?</h1>
-              <br />
-              <p>
-                Os Centros de Apoio Psicossocial (CAPS) desempenham um papel
-                crucial na promoção da saúde mental e no suporte às comunidades.
-                Eles são fundamentais para a desinstitucionalização do
-                tratamento psiquiátrico, oferecendo uma alternativa ao
-                internamento hospitalar. Os CAPS proporcionam um ambiente
-                acolhedor e humanizado, onde os pacientes podem receber
-                tratamento contínuo e integrado. Além disso, os CAPS trabalham
-                para combater o estigma associado às doenças mentais, promovendo
-                a inclusão social e o fortalecimento dos vínculos familiares e
-                comunitários
-              </p>
-              
+      <div className="container">
+        <table>
+          <tbody>
+            <tr>
+              <td className="td-direita">
+                <div className="apresentacao">
+                  <h1>Basicamente...</h1>
+                  <br />
+                  <p>
+                    O Centro de Atenção Psicossocial (CAPS) é uma unidade de
+                    saúde pública destinada ao atendimento de pessoas com
+                    transtornos mentais e necessidades de acompanhamento
+                    psicossocial. Os CAPS fazem parte do Sistema Único de Saúde
+                    (SUS) e têm como objetivo proporcionar cuidado contínuo,
+                    promover a reabilitação psicossocial e evitar internações
+                    hospitalares desnecessárias. Eles oferecem serviços que
+                    incluem consultas psiquiátricas, atendimento psicológico,
+                    atividades terapêuticas, oficinas de trabalho e exercicios
+                    em grupo. O CAPS atende tanto pessoas com transtornos
+                    mentais graves e persistentes quanto aquelas que necessitam
+                    de acompanhamento regular, como consultas mensais com
+                    psicólogos, sem estarem em situação de extremo risco.
+                  </p>
+                </div>
+              </td>
+              <td className="td-esquerda">
+                <img src={imagemInicio2} />
+              </td>
+            </tr>
+            <td className="td-direita">
+              <img src={imagemInicio} />
+            </td>
+            <td className="td-esquerda">
+              <div className="apresentacao-y">
+                <h1>E por que é importante?</h1>
+                <br />
+                <p>
+                  O CAPS é fundamental para a comunidade, pois oferece suporte
+                  essencial para pessoas com transtornos mentais, ajudando a
+                  melhorar a qualidade de vida desses indivíduos e de suas
+                  famílias. Os CAPS promovem a inclusão social e a cidadania,
+                  oferecendo um espaço de acolhimento e tratamento humanizado.
+                  Eles são importantes porque evitam a institucionalização,
+                  permitindo que os pacientes permaneçam em suas comunidades, e
+                  proporcionam atendimento multidisciplinar, garantindo uma
+                  abordagem abrangente para o tratamento de transtornos mentais.
+                </p>
               </div>
-        </td>
-        </tbody>
-      </table>
-      <br />
-      <br />
-    </div>
-    <div className="container-titulo">
-      <h1>Blogs</h1>
-      <br />
-      <h3>Falta escrever algum texto maneiro aqui</h3>
-      <br />
-      <br />
-    </div>
-    <div className="container-blog">
-      <div className="margem-blog">
-        <div className="menu-opcoes">
-          <div className="blog-item">
-            <a href="blogalcoolismo">
-              <img src={banned} />
-              <p>ALCOLISMO</p>
-            </a>
-          </div>
-          <div className="blog-item">
-            <a href="blogalcoolismo">
-              <img src={banned} />
-              <p>ALCOLISMO</p>
-            </a>
-          </div>
-          <div className="blog-item">
-            <a href="blogalcoolismo">
-              <img src={banned} />
-              <p>ALCOLISMO</p>
-            </a>
-          </div>
-          <div className="blog-item">
-            <a href="blogalcoolismo">
-              <img src={banned} />
-              <p>ALCOLISMO</p>
-            </a>
-          </div>
-          <div className="blog-item">
-            <a href="blogalcoolismo">
-              <img src={banned} />
-              <p>ALCOLISMO</p>
-            </a>
-          </div>
-          <div className="blog-item">
-            <a href="blogalcoolismo">
-              <img src={banned} />
-              <p>ALCOLISMO</p>
-            </a>
-          </div>
-          <div className="blog-item">
-            <a href="blogalcoolismo">
-              <img src={banned} />
-              <p>ALCOLISMO</p>
-            </a>
-          </div>
-          <div className="blog-item">
-            <a href="blogalcoolismo">
-              <img src={banned} />
-              <p>ALCOLISMO</p>
-            </a>
-          </div>
-        </div>
+            </td>
+            <tr>
+              <td className="td-direita">
+                <div className="apresentacao-r">
+                  <h1>E como surgiu isso ai?</h1>
+                  <br />
+                  <p>
+                    O CAPS surgiu como parte da reforma psiquiátrica no Brasil,
+                    que foi impulsionada após a criação do SUS em 1988. Essa
+                    reforma visava substituir o modelo manicomial por um modelo
+                    de cuidado mais humanizado e inclusivo. A partir dos anos
+                    1990, houve um movimento para desinstitucionalizar o
+                    atendimento psiquiátrico, promovendo a criação de serviços
+                    comunitários de saúde mental, como os CAPS. Essa mudança foi
+                    motivada pela necessidade de tratar pacientes com respeito e
+                    dignidade, focando na reabilitação psicossocial e na
+                    reintegração dos indivíduos na sociedade
+                  </p>
+                  <br />
+                </div>
+              </td>
+              <td className="td-esquerda">
+                <img src={mulher_falando} />
+              </td>
+            </tr>
+          </tbody>
+        </table>
         <br />
         <br />
       </div>
-    </div>
-
-    <div className="container-titulo">
-      <h1>Endereços</h1>
-      <br />
-      <h3>
-        Clicando nos icones você poderá ver mais informações sobre cada
-        instalção??
-      </h3>
-      <br />
-      <br />
-    </div>
-
- 
-    <div className="rodape">
-      <div className="margem-rodape">
+      <div className="container-titulo">
+        <h1>Blogs</h1>
         <br />
-        <div className="rodape-sobre-nos">
-          <a href='sobrenos'>
-          <h2>SOBRE NÓS</h2>
-          </a>
-          <ul>
-            <li>
-              Somos uma equipe dedicada em contribuir e informar a população,
-              sobre o CAPS, uma instituição que ajuda a população menos
-              privilegiada a ter acesso a atendimento psicologico
-            </li>
-          </ul>
-        </div>
-        <div className="rodape-contatos">
-          <h2>CONTATOS</h2>
-          <ul>
-            <li>
-              <a href="https://www.instagram.com/fasipe_grupo_caps/">
-                Nosso instagram<img src={instagram}
-              /></a>
-            </li>
-            <li>
-              <a href="https://www.gmail.com">
-                Nosso email<img src={email}
-              /></a>
-            </li>
-          </ul>
-        </div>
-        <div className="rodape-nossa-equipe">
-          <h2>NOSSA EQUIPE</h2>
-          <ul>
-            <li>José Fernando</li>
-            <li>Ana Sobrenome</li>
-            <li>Luis Sobrenome</li>
-            <li>Caio Sobrenome</li>
-            <li>Lucas Gustavo</li>
-            <li>Gustavo Waltric</li>
-            <li>Mais Gustavo</li>
-            <li>Gustavo 4</li>
-          </ul>
+        <h3>Outros assuntos importantes e aprofundamento sobre os temas</h3>
+        <br />
+        <br />
+      </div>
+      <div className="container-blog">
+        <div className="margem-blog">
+          <div className="menu-opcoes">
+            <div className="blog-item">
+              <a href="blogalcoolismo">
+                <img src={banned} />
+                <p>ALCOLISMO</p>
+              </a>
+            </div>
+            <div className="blog-item">
+              <a href="blogalcoolismo">
+                <img src={banned} />
+                <p>ALCOLISMO</p>
+              </a>
+            </div>
+            <div className="blog-item">
+              <a href="blogalcoolismo">
+                <img src={banned} />
+                <p>ALCOLISMO</p>
+              </a>
+            </div>
+            <div className="blog-item">
+              <a href="blogalcoolismo">
+                <img src={banned} />
+                <p>ALCOLISMO</p>
+              </a>
+            </div>
+            <div className="blog-item">
+              <a href="blogalcoolismo">
+                <img src={banned} />
+                <p>ALCOLISMO</p>
+              </a>
+            </div>
+            <div className="blog-item">
+              <a href="blogalcoolismo">
+                <img src={banned} />
+                <p>ALCOLISMO</p>
+              </a>
+            </div>
+            <div className="blog-item">
+              <a href="blogalcoolismo">
+                <img src={banned} />
+                <p>ALCOLISMO</p>
+              </a>
+            </div>
+            <div className="blog-item">
+              <a href="blogalcoolismo">
+                <img src={banned} />
+                <p>ALCOLISMO</p>
+              </a>
+            </div>
+          </div>
+          <br />
+          <br />
         </div>
       </div>
-    </div>
+
+      <div className="container-titulo">
+        <h1>Endereço e Contato</h1>
+        <br />
+        <h3>
+          Clique no botão e confira os endereços e mais informações dos CAPS na
+          região de Cuiabá
+        </h3>
+        <br />
+      </div>
+      <div className="endereco-item">
+        <a href="/localizacao">
+        <h2>CLIQUE AQUI E VERIFIQUE OS ENDERECOS</h2>
+        </a>
+      </div>
+        <br />
+        <br />
+        <br />
+      <div className="rodape">
+        <div className="margem-rodape">
+          <br />
+          <div className="rodape-sobre-nos">
+            <a href="sobrenos">
+              <h2>SOBRE NÓS</h2>
+            </a>
+            <ul>
+              <li>
+                Somos uma equipe dedicada em contribuir e informar a população,
+                sobre o CAPS, uma instituição que ajuda a população menos
+                privilegiada a ter acesso a atendimento psicologico
+              </li>
+            </ul>
+          </div>
+          <div className="rodape-contatos">
+            <h2>CONTATOS</h2>
+            <ul>
+              <li>
+                <a href="https://www.instagram.com/fasipe_grupo_caps/">
+                  Nosso instagram
+                  <img src={instagram} />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.gmail.com">
+                  Nosso email
+                  <img src={email} />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="rodape-nossa-equipe">
+            <h2>NOSSA EQUIPE</h2>
+            <ul>
+              <li>José Fernando</li>
+              <li>Ana Sobrenome</li>
+              <li>Luis Sobrenome</li>
+              <li>Caio Sobrenome</li>
+              <li>Lucas Gustavo</li>
+              <li>Gustavo Waltric</li>
+              <li>Mais Gustavo</li>
+              <li>Gustavo 4</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
--- src/localizacao.jsx
+++ src/localizacao.jsx
@@ -1,29 +1,156 @@
-import './localizacao.css'
+import "./localizacao.css";
+import CAPSI from "./assets/capsiv.png";
+import CAPSII from "./assets/capsii.png";
+import CAPSIII from "./assets/capsadolescer.png";
+import CAPSAD from "./assets/capsad.png";
+import CAPS from "./assets/caps.png";
+import instagram from "./assets/instagram.png";
+import email from "./assets/email.png";
 
 function Localizacao() {
+  return (
+    <>
+      <div className="title">
+        <a className="title" href="./">
+          <h1>CAPS - Centro de Apoio Psicossocial</h1>
+        </a>
+      </div>
+      <div className="container-titulo">
+        <h1>Blogs</h1>
+        <br />
+        <h3>Outros assuntos importantes e aprofundamento sobre os temas</h3>
+        <br />
+        <br />
+        </div>
+      <table>
+        <tr>
+          <td className="table-imagem">
+            <img src={CAPSI} />
+          </td>
+          <td className="table-texto">
+            <div className="box">
+              <h1>NOME: CAPS I</h1>
+              <br />
+              ENDERECO: RUA 82 - Morada da Serra, Cuiaba-MT
+              <br />
+              CEP:78052-530
+            </div>
+          </td>
+        </tr>
 
-    return (
-      <>
-      <h1>Lugar que</h1>
-<table>
-  <tr>
-    <td className='table-imagem'>
-        IMAGEM
-        </td>
-    <td className='table-texto'>
-        
-    <div className='box'>
-    NOME: CAPS III
-    <br/>
-    ENDERECO: RUA ARNALO 232
-    </div>
-        
-        </td>
-  </tr>
-</table>
-         
-     </>
-  )
-  }
+        <tr>
+          <td className="table-texto2">
+            <div className="box2">
+              NOME: CAPS II
+              <br />
+              ENDERECO: RUA MAURICIO CARDOSO, 735, CIDADE ALTA, CUIABA-MT
+              <br />
+              CEP: 78030-425
+            </div>
+          </td>
+          <td className="table-imagem2">
+            <img src={CAPSII} />
+          </td>
+        </tr>
 
-  export default Localizacao
+        <tr>
+          <td className="table-imagem3">
+            <img src={CAPSAD} />
+          </td>
+          <td className="table-texto3">
+            <div className="box3">
+              NOME: CAPS AD INFANTO JUVENIL
+              <br />
+              ENDERECO: RUA ROMÊNIA, JARDIM EUROPA, CUIABA-MT
+              <br />
+              CEP: 78065-395
+            </div>
+          </td>
+        </tr>
+
+        <tr>
+          <td className="table-texto4">
+            <div className="box4">
+              NOME: CAPSIII ADOLESCER
+              <br />
+              ENDERECO: RUA CEILÃO, 304, JARDIM SHANGRI-LÁ, CUIABA-MT
+              <br />
+              CEP: 78070-150
+            </div>
+          </td>
+          <td className="table-imagem4">
+            <img src={CAPSIII} />
+          </td>
+        </tr>
+
+        <tr>
+          <td className="table-imagem5">
+            <img src={CAPS} />
+          </td>
+          <td className="table-texto5">
+            <div className="box5">
+              NOME: CAPS
+              <br />
+              ENDERECO: RUA ADAUTLO BOTELHO - COOPHEMA, CUIABA-MT
+              <br />
+              CEP: 78085-200
+              <br />
+              CEP: 78065-395
+            </div>
+          </td>
+        </tr>
+      </table>
+      <br/>
+      <br/>
+      <br/>
+      <div className="rodape">
+        <div className="margem-rodape">
+          <br />
+          <div className="rodape-sobre-nos">
+            <a href="sobrenos">
+              <h2>SOBRE NÓS</h2>
+            </a>
+            <ul>
+              <li>
+                Somos uma equipe dedicada em contribuir e informar a população,
+                sobre o CAPS, uma instituição que ajuda a população menos
+                privilegiada a ter acesso a atendimento psicologico
+              </li>
+            </ul>
+          </div>
+          <div className="rodape-contatos">
+            <h2>CONTATOS</h2>
+            <ul>
+              <li>
+                <a href="https://www.instagram.com/fasipe_grupo_caps/">
+                  Nosso instagram
+                  <img src={instagram} />
+                </a>
+              </li>
+              <li>
+                <a href="https://www.gmail.com">
+                  Nosso email
+                  <img src={email} />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="rodape-nossa-equipe">
+            <h2>NOSSA EQUIPE</h2>
+            <ul>
+              <li>José Fernando</li>
+              <li>Ana Sobrenome</li>
+              <li>Luis Sobrenome</li>
+              <li>Caio Sobrenome</li>
+              <li>Lucas Gustavo</li>
+              <li>Gustavo Waltric</li>
+              <li>Mais Gustavo</li>
+              <li>Gustavo 4</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Localizacao;
